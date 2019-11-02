0f8b2434f75007abb24554e7c6b2c7bd167ee6eb
Rúben Mano <rumcmano@gmail.com>
2019-11-03 01:33:34 +0200

    Challenge completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,25 +11,50 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.write(`Hello Ruben`);
-  res.send();
+// Adding route for every post
+app.get("/posts/:post", (req, res) => {
+  for(let i = 0; i < posts.length; i++)
+    if(_.lowerCase(posts[i].title) === _.lowerCase(req.params.post))
+      res.render('post', {title: posts[i].title, post: posts[i].post});
 });
 
+// Adding route for the homePage
+app.get("/", (req, res) => {
+  res.render('home', {homeText: homeStartingContent, posts: posts});
+});
 
+// Adding route for the aboutPage
+app.get("/about", (req, res) => {
+  res.render('about', {aboutText: aboutContent});
+});
 
+// Adding route for the contactPage
+app.get("/contact", (req, res) => {
+  res.render('contact', {contactText: contactContent});
+});
 
+// Adding route for the compose page
+app.get("/compose", (req, res) => {
+  res.render('compose');
+});
 
-
-
-
-
-
+// Adding a post method for compose
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.title,
+    post: req.body.post
+  };
+  posts.push(post);
+  // It's redirect to the get method
+  res.redirect('/');
+});
 
 
 app.listen(3000, function() {
